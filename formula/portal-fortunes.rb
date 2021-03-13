63dc5f2dccbed2ccda993e40c4a16c650270a7aa
Baptiste Candellier <outadoc@gmail.com>
2021-03-13 16:31:46 +0100

    Fix :branch

--- formula/portal-fortunes.rb
+++ formula/portal-fortunes.rb
@@ -5,7 +5,7 @@ class PortalFortunes < Formula
   homepage "https://github.com/outadoc/portal-fortunes"
   url "https://github.com/outadoc/portal-fortunes/archive/v1.0.tar.gz"
   sha256 "610da6390a296d1126e0bfb01e2f6c2adfd34591de6105274365b6d8fd32ea92"
-  head "https://github.com/outadoc/portal-fortunes.git", branch => "develop"
+  head "https://github.com/outadoc/portal-fortunes.git", :branch => "develop"
 
   depends_on "fortune"
 
